57c49f2f8c6f775bb5b20d22046e4737fcc04786
Samuel Bancal <Samuel.Bancal@epfl.ch>
2015-03-26 17:15:15 +0100

    2015.03.26 fix py2app generation
    Generation of mount_filers.app for Mac OSX is done with the following 3 commands :
      python setup_osx.py py2app
      cp /Users/bancal/anaconda/lib/libQtCore.4.dylib dist/mount_filers.app/Contents/Resources/lib/
      cp /Users/bancal/anaconda/lib/libQtGui.4.dylib dist/mount_filers.app/Contents/Resources/lib/

--- client/setup_osx.py
+++ client/setup_osx.py
@@ -7,16 +7,19 @@ Usage:
 
 from setuptools import setup
 
-APP = ['mount_filers.py']
+APP = ["mount_filers.py"]
 DATA_FILES = ["mount_filers.png"]
 OPTIONS = {
-    'argv_emulation': True,
-    'iconfile': '/Users/bancal/SWITCHdrive/mount_filers/client/mount_filers.icns'
+    "argv_emulation": True,
+    # "frameworks": ["libQtCore.4.dylib", "libQtGui.4.dylib"],
+    "iconfile": "/Users/bancal/mount_filers_client/mount_filers.icns",
+    "includes": ["sip", "PyQt4", "PyQt4.QtCore", "PyQt4.QtGui"],
+    "qt_plugins": ["QtGui", "QtCore"]
 }
 
 setup(
     app=APP,
     data_files=DATA_FILES,
-    options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
+    options={"py2app": OPTIONS},
+    setup_requires=["py2app"],
 )
