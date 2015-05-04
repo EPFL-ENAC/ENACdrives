8506d8baa04412dc54c64c66b6a8b977104145e7
Samuel Bancal <Samuel.Bancal@epfl.ch>
2015-05-04 15:10:42 +0200

    2015-05-04 Start to document new release workflow

---
+++ docs/NEW_CLIENT_RELEASE.md
@@ -0,0 +1,36 @@
+% New client ENACdrives : Workflow
+% SB, ND, SN, PDJ (ENAC-IT)
+
+
+COMMON
+======
+
+* Project repo : https://_username_@git.epfl.ch/repo/enacdrives.git
+* Release version : set in enacdrives/client/utility.py (CONST.VERSION & CONST.FULL_VERSION)
+
+
+LINUX
+=====
+
+
+WINDOWS
+=======
+
+<SB>
+
+~~~ bash
+cp -R client/ /home/sbancal/Desktop/enac-it_on_enacfiles1/commun1/ENACdrives/src/client/
+~~~
+
+~~~
+cd Y:\commun1\ENACdrives\src\client
+python setup.py build
+~~~
+
+<ND>
+...
+
+
+
+MACOSX
+======
---
+++ docs/fabfile.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# Bancal Samuel
+# 2015-05-04
+
+# Usage :
+# fab deploy pandoc
+
+
+import os
+from fabric.api import run, env, task  # cd
+from fabric.contrib.project import rsync_project
+# from fabric.operations import sudo
+
+env.hosts = ['enacit1@enacit1-sysadmin']
+
+LOCAL_DIR = "/home/sbancal/Projects/enacdrives/docs/"
+REMOTE_DIR = "/data/web/enacit1-sysadmin/ENACdrives/"
+
+MD_FILES = ("NEW_CLIENT_RELEASE.md",)
+CSS = "/data/web/enacit1-sysadmin/markdown-pandoc.css"
+
+
+@task
+def ls():
+    run("ls -l %s" % REMOTE_DIR)
+
+
+@task
+def deploy():
+    rsync_project(
+        local_dir=LOCAL_DIR,
+        remote_dir=REMOTE_DIR,
+        exclude=("*.git", "*.pyc"),
+        # delete=True,
+        extra_opts="--links",
+    )
+
+
+@task
+def pandoc():
+    for f in [os.path.join(REMOTE_DIR, f)[:-3] for f in MD_FILES]:
+        run("pandoc -s --self-contained --toc --toc-depth=2 -c %s -o %s.html %s.md" % (CSS, f, f))
