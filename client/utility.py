ba6cf04169d7fe5e46636577ded7dd63d8f62ecd
Samuel Bancal <Samuel.Bancal@epfl.ch>
2015-05-21 16:09:36 +0200

    2015-05-21 client 0.2.0

--- client/utility.py
+++ client/utility.py
@@ -61,7 +61,7 @@ def which(program):
 class CONST():
 
     VERSION_DATE = "2015-05-21"
-    VERSION = "0.1.29"
+    VERSION = "0.2.0"
     FULL_VERSION = VERSION_DATE + " " + VERSION
 
     OS_SYS = platform.system()
