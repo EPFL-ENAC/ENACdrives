0c4af0be2d73e66d1bb0801a11aa0f6616825d18
Samuel Bancal <Samuel.Bancal@epfl.ch>
2016-04-29 15:00:25 +0200

    2016-04-29 +Vagrant OS setup

--- Vagrant/provision_script.sh
+++ Vagrant/provision_script.sh
@@ -2,27 +2,29 @@
 
 function title1 {
     string=$*
-    length=${#string}
-    echo "$@"
+    length=$(echo ${#string} + 10 | bc)
+    echo "---> $@ <---"
     printf %${length}s |tr " " "="
     echo -e "\n"
 }
 
 function title2 {
     string=$*
-    length=${#string}
-    echo "$@"
+    length=$(echo ${#string} + 10 | bc)
+    echo "---> $@ <---"
     printf %${length}s |tr " " "-"
     echo -e "\n"
 }
 
 function title3 {
-    echo "### $@"
+    echo "### ---> $@ <---"
     echo
 }
 
 
 title1 "Start provisioning"
+export DEBIAN_FRONTEND=noninteractive
+. /vagrant/secrets.sh
 
 title2 "Setup keyboard..."
 sed -i.bak 's/^XKBMODEL=.*/XKBMODEL="pc105"/' /etc/default/keyboard
@@ -39,15 +41,101 @@ title2 "Setup to use mirror.switch.ch"
 sed -i -e "s/http:\\/\\/archive.ubuntu.com/http:\\/\\/mirror.switch.ch\\/ftp\\/mirror\\/ubuntu/g" /etc/apt/sources.list
 
 title2 "update and dist-upgrade"
-apt-get -y update
-apt-get -y dist-upgrade
+apt-get -q -y update
+apt-get -q -y dist-upgrade
 
 title2 "purge chef and puppet"
-apt-get -y purge chef chef-zero puppet puppet-common
+apt-get -q -y purge chef chef-zero puppet puppet-common
+
+# Python env
+title2 "Install Server basics"
+apt-get -q -y install vim tree meld multitail ipython
+apt-get -q -y install build-essential python-dev python-pip
+
+title2 "Install Mail"
+apt-get -q -y install mailutils ssmtp
+install -b -o root -g root -m 644 /vagrant/etc/ssmtp/ssmtp.conf /etc/ssmtp
+install -b -o root -g root -m 644 /vagrant/etc/ssmtp/revaliases /etc/ssmtp
+
+title2 "Install NTP"
+apt-get -q -y install ntp
+install -b -o root -g root -m 644 /vagrant/etc/ntp.conf /etc
+/etc/init.d/ntp restart
+
+title2 "Setup UFW"
+ufw disable
+ufw --force reset
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow proto tcp from 10.0.2.0/24 to any port 22
+ufw allow proto tcp from 128.178.7.66 to any port 22
+ufw allow proto tcp from any to any port 80
+ufw --force enable
+ufw status
+
+title2 "Install Apache2"
+apt-get -q -y install apache2 apache2-dev apache2-threaded-dev
+install -b -o root -g root -m 644 /vagrant/etc/apache2/conf-available/servername.conf /etc/apache2/conf-available
+a2enconf servername
+service apache2 restart
+
+title2 "Install MySQL"
+apt-get -q -y install mysql-server mysql-client
+install -b -o root -g root -m 644 /vagrant/etc/mysql/conf.d/enacit.cnf /etc/mysql/conf.d/
+service mysql restart
+mysql -u root <<EOF
+create user 'enacdrives'@'localhost' identified by '${MYSQL_ENACDRIVES_PWD}';
+create database enacdrives;
+grant all privileges on enacdrives.* to 'enacdrives'@'localhost';
+commit;
+EOF
+mysqladmin -u root password ${MYSQL_ROOT_PWD}
+
+title2 "Install Tequila"
+wget http://tequila.epfl.ch/download/2.0/tequila-apache-C-2.0.16.tgz
+tar -xvzf tequila-apache-C-2.0.16.tgz
+pushd tequila-2.0.16/Apache/C/
+ed - Makefile <<EOFMakefile
+g/httpd /s:httpd :apache2 :
+g/apxs/s:apxs :apxs2 :
+wq
+EOFMakefile
+make
+make install
+popd
+
+title2 "Setup Tequila"
+install -b -o root -g root -m 644 /vagrant/etc/apache2/mods-available/tequila.load /etc/apache2/mods-available
+install -b -o root -g root -m 644 /vagrant/etc/apache2/mods-available/tequila.conf /etc/apache2/mods-available
+mkdir -p /var/tequila
+chown www-data: /var/tequila
+a2enmod tequila
+service apache2 restart
+apache2ctl -t -D DUMP_MODULES | grep tequila
+
+title2 "Install Django"
+mkdir -p /django_app
+chown vagrant\: /django_app
+apt-get -q -y install python-virtualenv python-pip python3-dev
+apt-get -q -y install libldap2-dev libsasl2-dev
+apt-get -q -y install libapache2-mod-xsendfile
+cat <<EOF
+... Now Deploy from salsa.epfl.ch
+~~~ bash
+cd /home/sbancal/Projects/enacdrives/web_app
+# pww
+/home/sbancal/py/2/bin/fab -H enacit1sbtest4 full_deploy
+~~~
+
+Check :
+<http://enacit1sbtest4.epfl.ch/config/admin>
+<http://enacit1sbtest4.epfl.ch/config/config>
+EOF
+
 
 title2 "Clean up"
-apt-get -y autoremove
-apt-get -y clean
-apt-get -y autoclean
+apt-get -q -y autoremove
+apt-get -q -y clean
+apt-get -q -y autoclean
 
 title1 "Finished !"
