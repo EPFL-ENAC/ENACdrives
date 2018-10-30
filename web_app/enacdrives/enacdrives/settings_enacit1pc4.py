073e0674d9896e626378dcc7b177cab8418ff246
Samuel Bancal <Samuel.Bancal@epfl.ch>
2018-10-30 15:55:05 +0100

    +Include IP in ALLOWED_HOSTS
    Some scan use the IP instead of the hostname ... this generates an email
    for every request!

--- web_app/enacdrives/enacdrives/settings_enacit1pc4.py
+++ web_app/enacdrives/enacdrives/settings_enacit1pc4.py
@@ -13,7 +13,7 @@ SECRET_KEY = '=-s95apy2p(w9tnk#-n=h76r1xag4x^g)tvak#w#cssh$h6br!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["enacit1pc4", "enacit1pc4.epfl.ch", "salsa", "salsa.epfl.ch"]
+ALLOWED_HOSTS = ["enacit1pc4", "enacit1pc4.epfl.ch", "salsa", "salsa.epfl.ch", "128.178.7.66"]
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
