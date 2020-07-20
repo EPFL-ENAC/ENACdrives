6c96679585870269c382e6efe46bd80f2c64794b
Samuel Bancal <Samuel.Bancal@epfl.ch>
2020-07-20 15:54:45 +0200

    [WIP](setup.py)→ For debmake

--- client/enacdrives/setup.py
+++ client/enacdrives/setup.py
@@ -1,48 +1,49 @@
 #!/usr/bin/env python3
 # Bancal Samuel
 
-import sys
 import utility
-from cx_Freeze import setup, Executable
-
-# Dependencies are automatically detected, but it might need
-# fine tuning.
-buildOptions = dict(
-    packages=[],
-    excludes=[],
-    include_files=[
-        "enacdrives.png", "mounted.png", "umounted.png", "bookmark_on.png",
-        "bookmark_off.png", "warning.png", "warning_48.png", "critical_48.png",
-        "info_48.png", "msg_48.png"],
-    include_msvcr=True  # skip error msvcr100.dll missing
-    # zip_include_packages="*",
-    # zip_exclude_packages=""
-)
-
-
-if sys.platform == "win32":
-    # target_dir = "build_windows"
-    base = "Win32GUI"  # Tells the build script to hide the console.
-elif sys.platform in ("linux", "linux2", "linux3"):
-    # target_dir = "build_linux"
-    base = None
-else:
-    raise Exception("Not managed platform %s" % sys.platform)
+from distutils.core import setup
 
 setup(
     name='ENACdrives',
     version=utility.CONST.VERSION,
     description='Access your Network Drives easily.',
-    options=dict(build_exe=buildOptions),
-    executables=[Executable(
-        script="enacdrives.py",
-        # initScript = None,
-        # targetDir = target_dir,
-        # targetName = "enacdrives.exe",
-        base=base,
-        # compress=True, unexpected keyword argument 2020-05-13
-        icon="enacdrives.ico"
-        # copyDependentFiles = False,
-        # appendScriptToExe = False,
-        # appendScriptToLibrary = False,
-    )])
+    long_description='Access your Network Drives easily.',
+    author='Samuel Bancal',
+    author_email='Samuel.Bancal@epfl.ch',
+    url='https://enacit.epfl.ch/enacdrives/',
+    packages=['enacdrives'],
+    package_dir={'enacdrives': 'enacdrives'},
+    scripts=['enacdrives/enacdrives.py'],
+    data_files=[
+        ('share/applications', ['meta/enacdrives.desktop']),
+        ('share/pixmaps',
+            ['pixmaps/bookmark_off.png',
+             'pixmaps/bookmark_on.png',
+             'pixmaps/critical_48.png',
+             'pixmaps/enacdrives.icns',
+             'pixmaps/enacdrives.ico',
+             'pixmaps/enacdrives.png',
+             'pixmaps/info_48.png',
+             'pixmaps/mounted.png',
+             'pixmaps/msg_48.png',
+             'pixmaps/umounted.png',
+             'pixmaps/warning_48.png',
+             'pixmaps/warning.png']),
+        # ('share/man/man1', ['man/hello.1']),
+    ],
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Environment :: X11 Applications :: Qt',
+        'Intended Audience :: End Users/Desktop',
+        'License :: Free To Use But Restricted',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ],
+    platforms='POSIX',
+    license='MIT License',
+)
